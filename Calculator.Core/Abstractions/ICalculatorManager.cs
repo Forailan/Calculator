a51428df9d56ee55dbbad64ee0b21ad060f9f7d0
i.demianov <jens88@mail.ru>
2018-11-13 21:45:39 +0200

    updated repo

---
+++ Calculator.Core/Abstractions/ICalculatorManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Core.Abstractions
+{
+    interface ICalculatorManager
+    {
+        float CalculateExpression(string expression);
+    }
+}
---
+++ Calculator.Core/Implementations/CalculatorManager.cs
@@ -0,0 +1,19 @@
+﻿using Calculator.Core.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Core.Implementations
+{
+    public class CalculatorManager : ICalculatorManager
+    {
+        #region Metods
+                
+        public float CalculateExpression(string expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
